36de17d62d4173c41a63c3963e21dd972371fe98
DIR\mannix.t.kwan <mannix.man@hotmail.com>
2021-12-01 01:47:16 +0800

    add start script

--- app.js
+++ app.js
@@ -167,6 +167,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err });
 })
 
-app.listen(3000, () => {
-    console.log("Servicing on port 3000")
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Servicing on port ${port}`)
 })
